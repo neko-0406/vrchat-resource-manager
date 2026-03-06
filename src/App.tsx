4829daa539d35c9ea88326d8637eabfe46f712c0
momiji <sateraito3696@gmail.com>
2026-03-06 15:34:49 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import "./App.css";
+import './App.css';
 
 export default function App() {
-
-  return (
-    <div className="bg-blue-300">aiueo</div>
-  );
+  return <div className='bg-blue-300'>aiueo</div>;
 }
