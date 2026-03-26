0c05b27d048f3f6ce99b24a0a93136f8556c4bcc
momiji <sateraito3696@gmail.com>
2026-03-26 17:53:35 +0900

    設定の雛形まで実装

--- src/components/app/AppSideBar.tsx
+++ src/components/app/AppSideBar.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import {
   Sidebar,
   SidebarContent,
@@ -10,6 +10,7 @@ import {
   useSidebar,
 } from '../ui/sidebar';
 import { LucideIcon, Shirt, PackagePlus, Settings } from 'lucide-react';
+import { NavLink, useLocation } from 'react-router';
 
 export interface MenuItem {
   title: string;
@@ -37,6 +38,14 @@ const menuItems: MenuItem[] = [
 
 export default function AppSideBar(): ReactNode {
   const { setOpen, open } = useSidebar();
+  const location = useLocation();
+
+  // 現在のパスに一致するメニューアイテムを検索
+  const activeItem = useMemo(() => {
+    return menuItems.find((item) => location.pathname.includes(item.url)) || menuItems[0];
+  }, [location.pathname]);
+
+  const HeaderIcon = activeItem.icon;
 
   return (
     <Sidebar
@@ -49,18 +58,17 @@ export default function AppSideBar(): ReactNode {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton>
-              {/*アプリアイコン*/}
-              <div></div>
+            <SidebarMenuButton size='lg'>
+              {/*現在の項目に応じたアイコンを表示*/}
+              <div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
+                <HeaderIcon className='size-4' />
+              </div>
               {
                 // 開閉状態に応じて表示・非表示切り替え
                 open && (
-                  <div>
-                    <span>
-                      VRChat
-                      <br />
-                      Resource-Manager
-                    </span>
+                  <div className='grid flex-1 text-left text-sm leading-tight'>
+                    <span className='truncate font-semibold'>VRChat</span>
+                    <span className='truncate text-xs'>Resource-Manager</span>
                   </div>
                 )
               }
@@ -73,12 +81,14 @@ export default function AppSideBar(): ReactNode {
           <SidebarMenu>
             {menuItems.map((item) => (
               <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild tooltip={item.title}>
-                  <a href={item.url} className='py-6'>
-                    <item.icon className='size-6!' />
-                    <span className='text-base font-medium'>{item.title}</span>
-                  </a>
-                </SidebarMenuButton>
+                <NavLink to={item.url}>
+                  {({ isActive }) => (
+                    <SidebarMenuButton tooltip={item.title} isActive={isActive} className='py-6'>
+                      <item.icon className='size-6!' />
+                      <span className='text-base font-medium'>{item.title}</span>
+                    </SidebarMenuButton>
+                  )}
+                </NavLink>
               </SidebarMenuItem>
             ))}
           </SidebarMenu>
