392908931daf7683160f7bbf8e21573e01d0cdd1
momiji <sateraito3696@gmail.com>
2026-03-26 18:58:47 +0900

    設定をファイルとしてロード、書き出しする処理を追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 mod app_setting;
 use crate::app_setting::AppSetting;
 use std::sync::Mutex;
-use tauri::{Manager, State};
+use tauri::{AppHandle, Manager, State};
 
 pub struct AppState {
     pub config: Mutex<AppSetting>,
@@ -14,10 +14,13 @@ fn get_config(state: State<'_, AppState>) -> AppSetting {
 }
 
 #[tauri::command]
-fn save_config(state: State<'_, AppState>, config: AppSetting) {
+fn save_config(app: AppHandle, state: State<'_, AppState>, config: AppSetting) -> Result<(), String> {
     let mut state_config = state.config.lock().unwrap();
-    *state_config = config;
-    // 本来はここでファイル保存も行う
+    *state_config = config.clone();
+
+    // ファイルに保存
+    config.save(&app)?;
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -25,10 +28,16 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .manage(AppState {
-            config: Mutex::new(AppSetting::default()),
+        .setup(|app| {
+            // 起動時にファイルを読み込み
+            let config = AppSetting::load(app.handle());
+            app.manage(AppState {
+                config: Mutex::new(config),
+            });
+            Ok(())
         })
         .invoke_handler(tauri::generate_handler![get_config, save_config])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
